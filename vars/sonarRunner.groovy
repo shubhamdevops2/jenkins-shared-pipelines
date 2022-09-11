5c205e940b610dadabdc0e57797112ec3dd02b90
shubhamdevops2 <shubhamd.devops.2@gmail.com>
2022-09-11 21:45:17 +0530

    sdsd

--- vars/sonarRunner.groovy
+++ vars/sonarRunner.groovy
@@ -97,7 +97,7 @@ void call(String mavenHome, String targetFile,String releaseVersion){
         try{
             stage("Sonar: Analysis"){
                 environment {
-                    scannerHome = tool 'sonarqube1'
+                    scannerHome = tool 'sonarqube'
                 }
                 withSonarQubeEnv('sonarqube'){
                     //sh "${mavenHome} -f ${targetPom} -gs ${mavenSettings} clean org.jacoco:jacoco-maven-plugin:prepare-agent install org.jacoco:jacoco-maven-plugin:report-aggregate org.jacoco:jacoco-maven-plugin:report org.sonarsource.scanner.maven:sonar-maven-plugin:3.8.0.2131:sonar -Dsonar.projectKey=${sonarKey} -Dsonar.projectName=\"${sonarProjectName}\" -B"
